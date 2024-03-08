8a616fbea559bbfaa14e6137ede6c892102bb839
AmandaTodero <amanda.todero032@gmail.com>
2024-03-08 09:28:35 -0500

    Added Header and Balance

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./Header";
+import { Balance } from "./Balance";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <IncomeExpenses />
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
